53bea14db70a17b4c038edbea56028235b75bd28
Jakub Korytko <jakub@korytko.me>
2025-06-09 21:36:49 +0200

    (WIP) add application components

--- src/components/NavBar/NavBar.pig.mjs
+++ src/components/NavBar/NavBar.pig.mjs
@@ -2,20 +2,19 @@ import { sleep } from "/modules/helpers.pig";
 
 const excludedRoutes = ["/signin", "/signup"];
 
+const updateCurrentPage = () =>
+  ($B.currentPage = {
+    home: $H.route === "/",
+    reports: $H.route === "/reports",
+    dashboardOrNew: $H.route === "/dashboard" || $H.route === "/report/add",
+  });
+
 const hiddenNavBarStyles = [
   ["margin-top", "-72px"],
   ["display", "none"],
 ];
 
-$H.route = $$(document.location.pathname);
-
 $H.observe("route");
-
-$B.currentPage = {
-  home: $H.route === "/",
-  reports: $H.route === "/reports",
-  dashboardOrNew: $H.route === "/dashboard" || $H.route === "/report/add",
-};
 
 $B.firstRender = $$(true);
 
@@ -25,36 +24,28 @@ if ($B.firstRender) {
   if (!$B.shouldDisplayNavBar) {
     $this.style.setProperty(...hiddenNavBarStyles.at(0));
   }
+  updateCurrentPage();
 
   $B.firstRender = false;
-}
+} else {
+  const shouldDisplayNavBarBasedOnNewRoute = !excludedRoutes.includes($H.route);
 
-const main = $element("main");
+  (async () => {
+    if ($B.shouldDisplayNavBar && !shouldDisplayNavBarBasedOnNewRoute) {
+      this.style.setProperty(...hiddenNavBarStyles.at(0));
+      await sleep(300);
+      this.style.setProperty(...hiddenNavBarStyles.at(1));
+    } else if (!$B.shouldDisplayNavBar && shouldDisplayNavBarBasedOnNewRoute) {
+      this.style.removeProperty("display");
+      requestAnimationFrame(() => this.style.removeProperty("margin-top"));
+    } else {
+      $element("main")?.style.setProperty("transform", "scaleX(0)");
+      await sleep(200);
+      updateCurrentPage();
+      $element("main")?.style.removeProperty("transform");
 
-main?.style.removeProperty("transform");
-
-const onPopState = async () => {
-  const newRoute = document.location.pathname;
-  const shouldDisplayNavBarBasedOnNewRoute = !excludedRoutes.includes(newRoute);
-
-  if ($B.shouldDisplayNavBar && !shouldDisplayNavBarBasedOnNewRoute) {
-    this.style.setProperty(...hiddenNavBarStyles.at(0));
-    await sleep(300);
-    this.style.setProperty(...hiddenNavBarStyles.at(1));
-  } else if (!$B.shouldDisplayNavBar && shouldDisplayNavBarBasedOnNewRoute) {
-    this.style.removeProperty("display");
-    requestAnimationFrame(() => this.style.removeProperty("margin-top"));
-  } else {
-    main?.style.setProperty("transform", "scaleX(0)");
-    await sleep(200);
-  }
-
+      console.log("got here", $element("main")?.style);
+    }
+  })();
   $B.shouldDisplayNavBar = shouldDisplayNavBarBasedOnNewRoute;
-  $H.route = newRoute;
-};
-
-window.addEventListener("popstate", onPopState);
-
-$onBeforeUpdate(() => {
-  window.removeEventListener("popstate", onPopState);
-});
+}
